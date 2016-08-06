a4bfbc3a71d1f751cece01053876616b9d35b0f0
Трофимов Денис <mail@denistrofimov.com>
2016-08-06 16:59:37 +0500

    to framework

---
+++ MaterialIconsSwift/MaterialIconsSwift.h
@@ -0,0 +1,19 @@
+//
+//  MaterialIconsSwift.h
+//  MaterialIconsSwift
+//
+//  Created by Трофимов Денис on 06.08.16.
+//  Copyright © 2016 Denis Trofimov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for MaterialIconsSwift.
+FOUNDATION_EXPORT double MaterialIconsSwiftVersionNumber;
+
+//! Project version string for MaterialIconsSwift.
+FOUNDATION_EXPORT const unsigned char MaterialIconsSwiftVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MaterialIconsSwift/PublicHeader.h>
+
+
